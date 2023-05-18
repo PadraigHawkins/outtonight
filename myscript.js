9b04a3dc2cf3db4de2daf23a3ff63bc96cfab42a
PadraigHawkins <107004777+PadraigHawkins@users.noreply.github.com>
2023-05-18 01:28:49 +0100

    Google maps link added to array list

--- myscript.js
+++ myscript.js
@@ -1,31 +1,16 @@
-function GetValue()
-{
-    var myarray= new Array(
-    "PYG",
-    "THE LONG HAUL",
-    "DEVITTS",
-    "HOGANS",
-    "GROGANS",
-    "THE CAMDEN",
-    "NO NAME BAR",
-    "CASSIDYS",
-    "MARY'S BAR",
-    "GRAVEDIGGERS",
-    "COBBLESTONE",
-    "WHELAN'S",
-    "P.MAC’S",
-    "TAPPED",
-    "RYANS BAR",
-    "4 DAME LANE",
-    "THE WOOL SHED",
-    "SIN É",
-    "FLANNERY'S",
-    "DOYLE'S",
-    "FERRYMAN",
-    "LIVING ROOM",
-    "MARKET BAR",
-    "BERNARD SHAW",
-    "JIMMY RABITTS");
-    var random = myarray[Math.floor(Math.random() * myarray.length)];
-    document.getElementById("message").innerHTML=random;
-}
+// list of pubs with associated hyperlinks
+const names = [
+    { name: 'PYG', link: 'https://goo.gl/maps/21zWP7ZK7Rqmn5kF6' },
+    { name: 'THE LONG HAUL', link: 'https://goo.gl/maps/3R3pHFJqwfR2eBgH6' },
+    { name: 'DEVITTS', link: 'https://goo.gl/maps/Jmyrcm62GjnS4TsK9' },
+    { name: 'GROGANS', link: 'https://goo.gl/maps/emtvwoQftMa8r6m17' },
+    { name: 'THE CAMDEN', link: 'https://goo.gl/maps/Wnj931UWRts2zWNCA' }
+  ];
+
+  function selectRandomName() {
+    const randomIndex = Math.floor(Math.random() * names.length);
+    const selectedName = names[randomIndex];
+
+    const selectedNameElement = document.getElementById('selectedName');
+    selectedNameElement.innerHTML = `<a href="${selectedName.link}">${selectedName.name}</a>`;
+  }
