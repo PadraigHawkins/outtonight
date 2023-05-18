658bbea95deb105f2cf54b28a47cbc4d70992e60
PadraigHawkins <107004777+PadraigHawkins@users.noreply.github.com>
2023-05-18 14:38:43 +0100

    New pubs added

--- myscript.js
+++ myscript.js
@@ -4,7 +4,18 @@ const names = [
     { name: 'THE LONG HAUL', link: 'https://goo.gl/maps/3R3pHFJqwfR2eBgH6' },
     { name: 'DEVITTS', link: 'https://goo.gl/maps/Jmyrcm62GjnS4TsK9' },
     { name: 'GROGANS', link: 'https://goo.gl/maps/emtvwoQftMa8r6m17' },
+    { name: 'NO NAME', link: 'https://goo.gl/maps/uJejbs9qJqRZbNKz6' },
+    { name: 'CASSIDYS', link: 'https://goo.gl/maps/rtJN7Hi7N1y8ioKV8' },
+    { name: 'MARYS BAR', link: 'https://goo.gl/maps/eCmThjCTtqQjVau49' },
+    { name: 'WHELANS', link: 'https://goo.gl/maps/ZDjDLsX1xnLqFk3P6' },
+    { name: 'P MACS', link: 'https://goo.gl/maps/VwCyfiqpeWcUNrvK7' },
+    { name: 'TAPPED', link: 'https://goo.gl/maps/XGagPobWsUWXoZH6A' },
+    { name: '4 DAME LANE', link: 'https://goo.gl/maps/mvN6jMD5rXSffhsQ8' },
+    { name: 'THE WOOL SHED', link: 'https://goo.gl/maps/t3sov5XTJtZMS4jE6' },
+    { name: 'SIN E', link: 'https://goo.gl/maps/vvcHKfdNimswsR5t8' },
+    { name: 'FLANNERYS', link: 'https://goo.gl/maps/rnCNfPHpLcpcV6o36' },
     { name: 'THE CAMDEN', link: 'https://goo.gl/maps/Wnj931UWRts2zWNCA' }
+    
   ];
 
   function selectRandomName() {
